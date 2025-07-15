1d1fc884adc9d2130ea5a6b5497062e9b0a15340
JoaoVictorMCosta <contato.joaovictormcosta@gmail.com>
2025-07-15 01:31:39 -0300

    Update exercise Order

--- myapp/src/com/jvmcosta/exercicios/order/app/App.java
+++ myapp/src/com/jvmcosta/exercicios/order/app/App.java
@@ -1,5 +1,56 @@
 package com.jvmcosta.exercicios.order.app;
 
-public class App {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
+import com.jvmcosta.entities.enums.OrderStatus;
+import com.jvmcosta.exercicios.order.entities.Client;
+import com.jvmcosta.exercicios.order.entities.Order;
+import com.jvmcosta.exercicios.order.entities.OrderItem;
+import com.jvmcosta.exercicios.order.entities.Product;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        System.out.println("Enter client data:");
+        System.out.print("Name: ");
+        String name = "Alex Green";
+        System.out.print("Email: ");
+        String email = "alex@gmail.com";
+        System.out.print("Birth date (DD/MM/YYYY): ");
+        String birthDate = "15/03/1985";
+
+        Order order;
+        
+        System.out.println("Enter order data: ");
+        System.out.println("Status:");
+        String status = "PROCESSING";
+        System.out.println("How many items to this order? ");
+
+        order = new Order(LocalDate.now(), OrderStatus.valueOf(status),new Client(name,email,birthDate));
+
+        int n = 2;
+        
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter #"+(i+1)+" item data: ");
+            System.out.print("Product name: ");
+            String itemName = sc.nextLine();
+            System.out.print("Product price: ");
+            double itemPrice = sc.nextDouble();
+            System.out.print("Quantity: ");
+            int quantity = sc.nextInt();
+            sc.nextLine(); 
+            order.addItem(new OrderItem(quantity, itemPrice, new Product(itemName,itemPrice)));
+        }
+
+        System.out.println();
+        System.out.println("ORDER SUMARY:");
+        System.out.println();
+        
+
+        sc.close();
+    }
 }
